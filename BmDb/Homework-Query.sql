a77a49a563a3646e086f714aacc00a1f5315d732
mchilders14 <childersmatt4@gmail.com>
2021-05-26 09:58:40 -0400

    adding homework

---
+++ BmDb/Homework-Query.sql
@@ -0,0 +1,11 @@
+-- Select all movies and sort by title
+SELECT * from movie ORDER BY TITLE;
+
+-- Select all movies and sort by Year asc
+SELECT * from movie ORDER BY year asc;
+
+-- Select all actors sorted by birthdate desc
+SELECT * from actor ORDER BY Birthdate desc;
+
+-- Select scarlet Johansson's record by id
+SELECT * from actor WHERE ID = 3;
